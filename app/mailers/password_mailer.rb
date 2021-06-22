ab2dda9858261517a93534090ec1197c8b157150
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 18:28:53 -0300

    add @url in PasswordMailer

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,7 +2,7 @@ class PasswordMailer < ApplicationMailer
 
   def confirmation
     @user=params[:user]
-    
+    @url = params[:url]
 
     mail(to: @user.email, subject: "Confirmação de conta")
   end
