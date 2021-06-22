19672301ff0aee6a7552285ce6ff393806bf8d51
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 02:37:40 -0300

    create def current_user in AplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  def current_user
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header.present?
+
+    return nil unless header.present?
+    @decoded = JsonWebToken.decode(header)
+    return nil unless @decoded
+    
+    user = User.find_by(id: @decoded[0]['user_id'])
+    return user
+  end
 end
