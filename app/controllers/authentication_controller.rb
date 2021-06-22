3bd3fa249d82a610ab85aa584d7dc912bed3247d
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 19:04:11 -0300

    created confirm account in authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -22,25 +22,25 @@ class AuthenticationController < ApplicationController
     end
   end
 
-  #def confirm
-  #  if params[:validation_token].present?
+  def confirm
+    if params[:validation_token].present?
 
-  #    @user = User.find_by(validation_token: params[:validation_token])
-  #    if @user.present?
-  #      if @user.validate_user?(params[:validation_token])
-  #        render json: @user
-  #      else 
-  #        render json: {error: "Esse token já expirou"}, status: 422
-  #      end
+      @user = User.find_by(validation_token: params[:validation_token])
+      if @user.present?
+        if @user.validate_user?(params[:validation_token])
+          render json: @user
+        else 
+          render json: {error: "Esse token já expirou"}, status: 422
+        end
 
-  #    else
-  #      render json: {error: "Usuário não existe"}, status: 404
-  #    end
+      else
+        render json: {error: "Usuário não existe"}, status: 404
+      end
 
-  #  else
-  #    render json: {error: "token não presente"}, status: 400
-  #  end
-  #end
+    else
+      render json: {error: "token não presente"}, status: 400
+    end
+  end
 
   def repeat_validation_token
     @user = User.find_by(email: params[:email])
