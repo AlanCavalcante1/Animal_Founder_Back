ccd0243b629a65adffb61d231343affdf8316f98
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 18:30:21 -0300

    create def generation_validation_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_create :generation_validation_token
+
+  def generation_validation_token
+    
+    self.validation_token = generate_random_token
+    self.validation_token_expiry_at = Time.now + 2.days
+    
+  end
+
+  def validate_user?(token)
+    if validation_token_expiry_at > Time.now
+      self.is_valid = true
+      self.validation_token = nil
+      return true if self.save
+    else
+      return false
+    end
+  end
+
+  private
+    def generate_random_token
+      return SecureRandom.alphanumeric(15)
+    end
 end
