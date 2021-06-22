91df526d8107fd4b0b20c8a670643fba75af1a58
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 18:23:50 -0300

    create routes to send again token

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post '/sign_up', to: 'authentication#sign_up'
   post '/login', to: 'authentication#login'
+  #post '/confirmate'
+  post '/authentication/repeat_token', to: "authentication#repeat_validation_token"
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
