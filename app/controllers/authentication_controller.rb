c870b93271b88e147aeadb657bb3fbf40e730896
Alan Cavalcante <alan.lima@injunior.com.br>
2021-06-22 01:51:56 -0300

    create def sign_up

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,8 +1,20 @@
 class AuthenticationController < ApplicationController
-  
+
   def sign_up
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def login
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :cellphone, :password, :password_confirmation)
+    end
 end
